0f0f276de75f2e5e2124679a5eac94e32945df97
Khoa Cao <khoacao0601@gmail.com>
2021-02-12 23:42:34 -0800

    Update index.jsx

--- src/components/TodoForm/index.jsx
+++ src/components/TodoForm/index.jsx
@@ -1,6 +1,14 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
+TodoFrom.propTypes = {
+    onSubmit: PropTypes.func,
+};
+
+TodoFrom.defaultProps = {
+    onSubmit: null,
+}
+
 function TodoFrom(props) {
     const { onSubmit } = props;
     const [value, setValue] = useState('');
