1c3272a5461a9eab447af30f9b7a9e72690949cd
Khoa Cao <khoacao0601@gmail.com>
2021-02-22 23:22:28 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,3 @@
-import ColorBox from './components/ColorBox';
-import React, { useState } from 'react';
-import './App.css';
-import TodoList from './components/TodoList';
-import TodoFrom from './components/TodoForm';
-import Menu from './components/MyOwnPractice/index ';
-import PostList from './components/PostList';
-import Pagination from './components/Pagination';
 
 function App() {
   const [todoList, setTodoList] = useState([
